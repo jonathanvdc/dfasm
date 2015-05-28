140270a8909f4acc08671f9f4d32803a5e1b22af
Mauris Van Hauwe <0nooodl@gmail.com>
2015-05-28 17:02:53 +0200

    Add COM "Hello, World!" test

--- src/dfasm/comTests/helloWorld.asm
+++ src/dfasm/comTests/helloWorld.asm
@@ -1,10 +1,9 @@
 
 
-;mov  dx, msg      ; the address of our message in dx
-;mov  ah, 9        ; ah=9 - "print string" sub-function
-;int  33           ; call dos services
-
-mov  ah, 75       ; "terminate program" sub-function
+mov  dx, msg      ; the address of our message in dx
+mov  ah, 9        ; ah=9 - "print string" sub-function
 int  33           ; call dos services
+mov  ah, 0
+int  33
 
-;msg: .byte "Hello, World!", 13, 10, "$"   ; $-terminated message
+msg: .byte "Hello, World!", 13, 10, "$"   ; $-terminated message
